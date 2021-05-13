5cefc9c6427dd2bfff6a27eb189027b1b66dfbc9
nathancooperjones <nathancooperjones@gmail.com>
2021-05-13 16:26:32 -0500

    rename BasePipeline.hparams.n_epochs_completed_

--- collie_recs/model/base.py
+++ collie_recs/model/base.py
@@ -286,6 +286,7 @@ class CollieTrainerMinimal():
         self.best_epoch_loss = (0, sys.maxsize)
         self.train_steps = 0
         self.val_steps = 0
+        self.n_epochs_completed = 0
 
         if self.gpus is None or self.gpus is False or self.gpus == 0:
             self.device = 'cpu'
@@ -313,8 +314,7 @@ class CollieTrainerMinimal():
             self.first_run_pre_training_setup_complete_ = True
 
         # set up top-level epoch progress bar
-        epoch_iterator = range(model.hparams.n_epochs_completed_ + 1,
-                               model.hparams.n_epochs_completed_ + 1 + self.max_epochs)
+        epoch_iterator = range(self.n_epochs_completed + 1, self.max_epochs + 1)
         if self.verbosity >= 2:
             epoch_iterator = tqdm(epoch_iterator,
                                   position=0,
@@ -351,7 +351,8 @@ class CollieTrainerMinimal():
             if self.verbosity >= 1:
                 print(epoch_summary)
 
-            model.hparams.n_epochs_completed_ += 1
+            model.hparams.n_epochs_completed += 1
+            self.n_epochs_completed += 1
 
             # early stopping logic
             if (
@@ -650,7 +651,7 @@ class BasePipeline(LightningModule, metaclass=ABCMeta):
 
             self.hparams.num_users = self.train_loader.num_users
             self.hparams.num_items = self.train_loader.num_items
-            self.hparams.n_epochs_completed_ = 0
+            self.hparams.n_epochs_completed = 0
 
             self._configure_loss()
 
@@ -886,7 +887,7 @@ class BasePipeline(LightningModule, metaclass=ABCMeta):
         This method will be called after ``training_step``.
 
         """
-        self.hparams.n_epochs_completed_ += 1
+        self.hparams.n_epochs_completed += 1
 
         try:
             avg_loss = torch.stack([x['loss'] for x in outputs]).mean()
